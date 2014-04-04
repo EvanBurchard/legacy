f6d568bd7d3cbf183f38cd31e62498125a24ce38
evanburchard <evan.burchard@gmail.com>
2014-04-04 05:13:32 -0700

    added some concerns

---
+++ app/models/concerns/all.rb
@@ -0,0 +1,6 @@
+"Are people from that moon with the oceans going to tell our dolphins anything bad about us?"
+"my TV could be bigger"
+"Death"
+"Aaron from marketing is a dick"
+"Still haven't had a KFC double down"
+"This app doesn't have a theme song"

fabf68635a4b9502dce61d0d506b5cc0fcd5fe39
Rich Jones <rich@anomos.info>
2015-09-29 11:46:39 -0700

    Know you're cooking with microservices\!

---
+++ config.js
@@ -0,0 +1,14 @@
+{
+  "datacenter": "east-aws",
+  "data_dir": "/opt/consul",
+  "log_level": "INFO",
+  "node_name": "foobar",
+  "server": true,
+  "watches": [
+    {
+        "type": "checks",
+        "handler": "/usr/bin/health-check-handler.sh"
+    }
+  ]
+}
+
